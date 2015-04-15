d414837bcab5e4e1ba256ec9cac6f67e829b6b36
jdanzig <jdanzig@users.noreply.github.com>
2015-04-15 01:03:04 -0500

    Update dice_controller.rb
    Dice can now have the same number

--- hw/app/controllers/dice_controller.rb
+++ hw/app/controllers/dice_controller.rb
@@ -2,7 +2,7 @@ class DiceController < ApplicationController
   def roll
   	@roll_num = params[:roll_num].to_i + 1
   	@goal = params[:goal].to_i if @roll_num > 1
-  	@numbers = [1,2,3,4,5,6].sample(2)
+  	@numbers = [1,1,2,2,3,3,4,4,5,5,6,6].sample(2)
   	@sum = @numbers.inject(:+) # http://ruby-doc.org/core-2.2.0/Enumerable.html#method-i-inject
   	if [7,11].include?(@sum) && @roll_num == 1
   		@result = :win
